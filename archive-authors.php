4af118966596c2700c9b64cd176d3a18f93e89b2
Ken <kensmash@gmail.com>
2020-08-06 14:01:23 -0400

    add articles to author archives page

---
+++ archive-authors.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying author archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Georgia_Workers’_Compensation
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main container">
+    <div class="col-md-8 col-lg-9 my-5">
+
+        <?php if ( have_posts() ) : ?>
+
+        <header class="page-header">
+            <?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+		
+				?>
+        </header><!-- .page-header -->
+
+        <?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content-authors' );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+    </div>
+
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
---
+++ template-parts/content-authors.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template part for displaying authors
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Georgia_Workers’_Compensation
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+	</header>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+
+	<?php 
+
+	/*
+	*  Query posts for a relationship value.
+	*  This method uses the meta_query LIKE to match the string "123" to the database value a:1:{i:0;s:3:"123";} (serialized array)
+	*/
+
+	$articles = get_posts(array(
+		'post_type' => 'articles',
+		'meta_query' => array(
+			array(
+				'key' => 'authors', // name of custom field
+				'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+				'compare' => 'LIKE'
+			)
+		)
+	));
+
+	?>
+	<?php if( $articles ): ?>
+	<ul>
+		<?php foreach( $articles as $article ): ?>
+
+		<li>
+			<a href="<?php echo get_permalink( $article->ID ); ?>"><?php echo get_the_title( $article->ID ); ?></a>
+		</li>
+
+		<?php endforeach; ?>
+	</ul>
+	<?php endif; ?>
+
+</article>
